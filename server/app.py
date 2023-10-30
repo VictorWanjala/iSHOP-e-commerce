6d3aa81e756768265defa43d6a7a613c8989d7ea
+ <+>
2023-10-30 23:12:31 +0300

    added seed.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db ,User, Cart,Product
+from models import db ,User, Review,Product
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///app.db'
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+
 
 
 if __name__ =='__main__':
